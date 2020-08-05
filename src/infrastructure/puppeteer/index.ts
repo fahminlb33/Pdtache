5227ed6a2c50ab9882b752383b8328eab47a9045
fahminlb33 <fahminlb33@gmail.com>
2020-08-06 06:40:10 +0700

    Added puppeteer service

---
+++ src/infrastructure/puppeteer/index.ts
@@ -0,0 +1,23 @@
+import puppeteer from 'puppeteer';
+import config from '../config';
+
+export default class PuppeteerWrapper {
+
+  static async CreatePuppeteer(): Promise<puppeteer.Browser> {
+
+    if (config.isInDocker){
+      return await puppeteer.launch({ args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ]});
+    }
+    else
+    {
+      return await puppeteer.launch();
+    }
+
+  }
+
+}
+
