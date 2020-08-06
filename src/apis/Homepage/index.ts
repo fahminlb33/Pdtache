fa6e495859ffde89335b0b5622ea581fb66ddd76
fahminlb33 <fahminlb33@gmail.com>
2020-08-06 11:26:48 +0700

    Remove mustache-express deps

--- src/apis/Homepage/index.ts
+++ src/apis/Homepage/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
 
 export async function Homepage(_req: express.Request, res: express.Response): Promise<void> {
-  res.render('index');
+  res.sendFile('index.html', {root: `${__dirname}/../../../web` });
 }
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import mustacheExpress from 'mustache-express';
 import bodyParser from 'body-parser';
 
 export default async ({app}: { app: express.Application }): Promise<express.Application> => {
@@ -11,9 +10,6 @@ export default async ({app}: { app: express.Application }): Promise<express.Appl
   });
 
   app.enable('trust proxy');
-  app.set('views', `${__dirname}/../../views`);
-  app.set('view engine', 'mustache');
-  app.engine('mustache', mustacheExpress(`${__dirname}/../../web/views`, '.mustache'));
 
   app.use(express.static(`${__dirname}/../../web/assets`));
   app.use(bodyParser.urlencoded({extended: false}));
