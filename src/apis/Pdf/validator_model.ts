7625a30dba6802837c5370bdaf93edfd8b622f68
fahminlb33 <fahminlb33@gmail.com>
2020-08-06 11:26:29 +0700

    Fixed empty string validation

--- src/apis/Pdf/validator_model.ts
+++ src/apis/Pdf/validator_model.ts
@@ -1,18 +1,17 @@
 import joi from 'joi';
-import Joi from 'joi';
 
 export const GeneratePdfStoreSchema = joi.object({
-  html_url: joi.string().when('html_raw', {is: joi.exist(), then: Joi.optional(), otherwise: joi.required()}),
-  html_raw: joi.string().optional(),
+  html_raw: joi.string().allow('').optional(),
+  html_url: joi.string().allow('').optional(),
   body: joi.object().required()
 });
 
 export const GeneratePdfEphemeralSchema = joi.object({
-  html_url: joi.string().when('html_raw', {is: joi.exist(), then: Joi.optional(), otherwise: joi.required()}),
-  html_raw: joi.string().optional(),
+  html_raw: joi.string().allow('').optional(),
+  html_url: joi.string().allow('').optional(),
   body: joi.object().required()
 });
 
 export const DeletePdfSchema = joi.object({
-  documentId: joi.string().uuid().required()
+  documentId: joi.string().required()
 });
